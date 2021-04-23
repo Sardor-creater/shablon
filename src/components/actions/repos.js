0cfea148455b82449aaa6d8bc7ba20ca5540b48d
sardor-creater@yandex.com <sardor-creater@yandex.com>
2021-04-23 09:41:53 +0300

    without plock

--- src/components/actions/repos.js
+++ src/components/actions/repos.js
@@ -1,9 +1,9 @@
-import axios from 'axios';
-import { setRepos } from '../../reducers/newReducer';
+import axios from "axios";
+import { setRepos } from "../../reducers/newReducer";
 
 export const getCat = () => {
-    return async (dispatch)=>{
-        const response = await axios.get(`https://api.github.com/users`)
-        dispatch(setRepos(response.data))
-    }
-}
+	return async (dispatch) => {
+		const response = await axios.get(`https://api.github.com/users`);
+		dispatch(setRepos(response.data));
+	};
+};
